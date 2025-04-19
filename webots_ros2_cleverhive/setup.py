921dda213259758fa6ee876a2233196cfe453835
Szymon Bednorz <dsonyyy@gmail.com>
2025-04-19 13:59:33 +0200

    remove rosbot xl files from webots_ros2_cleverhive

--- webots_ros2_cleverhive/setup.py
+++ webots_ros2_cleverhive/setup.py
@@ -5,13 +5,11 @@ data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name + '/launch', ['launch/rosbot_launch.py']))
 data_files.append(('share/' + package_name + '/resource', ['resource/rosbot_controllers.yaml']))
-data_files.append(('share/' + package_name + '/resource', ['resource/rosbot_xl_controllers.yaml']))
 data_files.append(('share/' + package_name + '/resource', ['resource/ekf.yaml']))
 data_files.append(('share/' + package_name + '/resource', ['resource/laser_filter.yaml']))
 data_files.append(('share/' + package_name + '/resource', ['resource/rosbot_webots.urdf']))
 data_files.append(('share/' + package_name + '/resource', ['resource/rosbot_links_remappings.yaml']))
 data_files.append(('share/' + package_name + '/worlds', ['worlds/rosbot.wbt']))
-data_files.append(('share/' + package_name + '/worlds', ['worlds/rosbot_xl.wbt']))
 data_files.append(('share/' + package_name + '/worlds/meshes', ['worlds/meshes/husarion_world.dae']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
